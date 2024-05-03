f055fb83ef9b8cc778b407479bb2eb69b26b2159
cristoferamachi <amachicristofer@gmail.com>
2024-05-03 18:22:18 -0500

    Area de ataque de personaje con punto pivote (Marker2d)

--- characters/ninja.gd
+++ characters/ninja.gd
@@ -7,12 +7,11 @@ const SPEED = 30000
 # as the character moves.
 var idle_animation = ""
 
-func move_character(delta):
-	var direction = Input.get_vector("ui_left", "ui_right",  "ui_up", "ui_down")
-	velocity = direction * SPEED * delta
-	move_and_slide()
+var current_direction = ""
 
-func play_walking_animations():	
+var direction_vector = Vector2.ZERO
+
+func play_walking_animation():	
 	# Diagonal movements will end in either left or right
 	# walking animation.
 	# Play the proper walking animation
@@ -29,17 +28,83 @@ func play_walking_animations():
 	elif velocity.y < 0:
 		idle_animation = "idle_back"
 		sprite_2d.play("walking_back")
+		
+	#direction_vector = Vector2(direction_vector.x, -1)
+
+func move_character(delta):
+	var direction = Input.get_vector("ui_left", "ui_right",  "ui_up", "ui_down")
+	velocity = direction * SPEED * delta
+	move_and_slide()
 
 func play_idle_animations():
-	# Play the proper idle_animation
-	if abs(velocity.x) == 0 and abs(velocity.y) == 0:
-		if idle_animation != "":
-			sprite_2d.animation = idle_animation
+	# Play the proper idle_animation	
+	sprite_2d.animation = idle_animation
+
+func update_direction_vector():
+	# Sets what direction does 
+	# the player move
+	
+	# Refresh
+	direction_vector = Vector2.ZERO
+	
+	# X Axis
+	if velocity.x > 0:
+		# Right
+		direction_vector = Vector2(1, direction_vector.y)
+	elif velocity.x < 0:
+		# Left
+		direction_vector = Vector2(-1, direction_vector.y)
+
+	# Y Axis
+	if velocity.y > 0:
+		# Right
+		direction_vector = Vector2(direction_vector.x, 1)
+	elif velocity.y < 0:
+		# Left
+		direction_vector = Vector2(direction_vector.x, -1)
+
+func update_attack_area_rotation():
+	# Rotate Marker2d depending on
+	# the 8 directions
+	
+	if direction_vector == Vector2(0, -1):
+		# Up
+		$AttackAreaPivot.rotation_degrees = 180
+	elif direction_vector == Vector2(1, -1):
+		# Up right
+		$AttackAreaPivot.rotation_degrees = -135
+	elif direction_vector == Vector2(1, 0):
+		# Right
+		$AttackAreaPivot.rotation_degrees = -90
+	elif direction_vector == Vector2(1, 1):
+		# Down right
+		$AttackAreaPivot.rotation_degrees = -45
+	elif direction_vector == Vector2(0, 1):
+		# Down
+		$AttackAreaPivot.rotation_degrees = 0
+	elif direction_vector == Vector2(-1, 1):
+		# Down left
+		$AttackAreaPivot.rotation_degrees = 45
+	elif direction_vector == Vector2(-1, 0):
+		# Left
+		$AttackAreaPivot.rotation_degrees = 90
+	elif direction_vector == Vector2(-1, -1):
+		# Up left
+		$AttackAreaPivot.rotation_degrees = 135
 
 func _physics_process(delta):
-	# Movement manager
+	# Move character
 	move_character(delta)
-	# Walking and idle animations managers
-	play_walking_animations()
-	play_idle_animations()
+	# Play the proper animation
+	play_walking_animation()
+	
+	# If idle
+	if abs(velocity.x) == 0 and abs(velocity.y) == 0:
+		if idle_animation != "":
+			direction_vector = Vector2.ZERO
+			play_idle_animations()
+	
+	# Control attack area orientation
+	update_direction_vector()
+	update_attack_area_rotation()
 	
